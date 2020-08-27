c36451715b3f2d95852c943bb32700ce63de3f1b
YasenWang <00000000@wangyusheng.cn>
2020-08-27 17:10:02 +0800

    Adding project auto update sh file

--- runner.sh
+++ runner.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 #Author: Yasen Wang
 
-#====variable====
+#====VARIABLE STAGE====
 VIRTUAL_PATH='/data/env/bin'
 WEB_ROOT='/data/wwwroot/counsellor'
 SH_NAME='COUNSELLOR RUNNER'
+
+
+#====FUNCTION STAGE====
 #config virtual env.
 function env() {
   if [ -d "$VIRTUAL_PATH" ];then
     cd "$VIRTUAL_PATH" || return
       source activate
   else
-    echo 'Error:Failed to set virtual Environment.'
+    echo "Error:Failed to set virtual Environment."
   fi
 }
-
 #start project when physic server is restart.
 function start() {
   echo ''
 }
-
-#updata project when github has changed.
+#update project when github has changed.
 function update() {
-   echo ''
+  echo "Updating project from Git Server..."
+  git pull
 }
+
+
+#====MAIN STAGE====
+echo "Working SH file $0"
+if [ '-start' == "$1" ];then
+  start
+elif [ '-update' == "$1" ]; then
+  update
+fi
+
+
