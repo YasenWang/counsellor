437959514e28aebc3f684e66f51a5a24bcd226cc
YasenWang <00000000@wangyusheng.cn>
2020-09-03 15:28:28 +0800

    add wechat robot

--- dingtalk_robot/views.py
+++ dingtalk_robot/views.py
@@ -9,8 +9,7 @@ import requests
 
 
 def dingtalk_robot(request):
-    am = AppManager(2784001, 'dingekcnjw1fgxyc5lee', 'pmtnRMPZfHFI9kGU9mcIUbJbGvvY_1KG3U5HAf5Cz02lIi-TZ6JZLr0Ye7bPGvN2')
-    ms = MessageSender(am).get_text_sender('收到消息')
-    return HttpResponse(ms.data)
+
+    return HttpResponse('success!')
 
 
